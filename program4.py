c8a040b915dfe28b7b0dad8285c5d67046431d0e
Fishy <99276062+fishymcbites@users.noreply.github.com>
2023-09-05 15:46:34 -0500

    Update program4.py

--- program4.py
+++ program4.py
@@ -1,14 +1,35 @@
+#argument is the file??
+x = "/home/student/pie_time/data.csv"
 countryNames = []
 countryCodes = []
 
-
-def read_csv(x):
-    with open("/home/student/pie_time/data.csv","r") as country:
+#opens and reads lines into a variable
+def read_csv(file1):
+    with open(file1,"r") as country:
         lines = country.readlines()
         return lines
 
+
+
 def parse_csv(y):
-    for i in read_csv(lines):
+    for i in read_csv(x):
         newlist = i.split(",")
         countryNames.append(newlist[0])
         countryCodes.append(newlist[1])
+        
+
+def print_data():
+    for i in range(len(countryNames)):
+        print("-------")
+        print("Country Name: " + countryNames[i])
+        print("Country Code: " + countryCodes[i])
+
+
+
+a = read_csv(x)
+parse_csv(a)
+print_data()
+
+
+
+
